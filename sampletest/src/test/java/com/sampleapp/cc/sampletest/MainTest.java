0edbccfb19d7595ad44024583ee1d5c690aea7fd
Alexey Kazakovskiy <trafficreg@gmail.com>
2016-12-13 17:45:10 +0200

    Adding test examples

--- sampletest/src/test/java/com/sampleapp/cc/sampletest/MainTest.java
+++ sampletest/src/test/java/com/sampleapp/cc/sampletest/MainTest.java
@@ -1,12 +1,27 @@
 package com.sampleapp.cc.sampletest;
 
 import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 public class MainTest {
 
-    @Test
+    @Test(priority = 10)
     public void mytest() {
+        Main m = new Main();
+        Assert.assertNotNull(m);
+    }
+
+    @Test(priority = 20)
+    public void mytestSkipped() {
+        Main m = new Main();
+        if (m != null) {
+            throw new SkipException("This test has been skipped by condition");
+        }
+    }
+
+    @Test(priority = 30)
+    public void mytestFinish() {
         Main m = new Main();
         Assert.assertNotNull(m);
     }
